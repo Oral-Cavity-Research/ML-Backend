d6ee03046323893fb6a801bb37f64a9f167ce9b3
dinuransika <e17072@eng.pdn.ac.lk>
2023-07-10 14:04:59 +0530

    Added proper error codes

--- app.py
+++ app.py
@@ -11,35 +11,36 @@ app = Flask(__name__)
 load_dotenv()
 CORS(app)
 
-model = tf.keras.models.load_model(os.getenv('H5_FILE_PATH'))
 img_height = 224
 img_width = 224
 
 @app.route('/classify', methods=['POST'])
 def classify():
-    fileName = request.json['filename']
-    supported_extensions = ['.jpg', '.jpeg', '.png', '']
-    
-    image_path = None
+    try:
+        model = tf.keras.models.load_model(os.getenv('H5_FILE_PATH'))
+        filename = request.json['filename']
+        supported_extensions = ['.jpg', '.jpeg', '.png', '']
+        
+        image_path = None
 
-    for ext in supported_extensions:
-        path = os.getenv('IMAGES_PATH') + fileName + ext
-        if os.path.isfile(path):
-            image_path = path
-            break
-    
-    if image_path is None:
-        return jsonify({'error':'Image not found'})
-    
-    img = Image.open(image_path)
+        for ext in supported_extensions:
+            path = os.getenv('IMAGES_PATH') + filename + ext
+            if os.path.isfile(path):
+                image_path = path
+                break
+        
+        if image_path is None:
+            return jsonify({'error': 'Image not found'}), 404
+        
+        img = Image.open(image_path)
+        img = img.resize((img_height, img_width))
+        img_array = np.reshape(img, (1, img_height, img_width, 3))
+        pred = model.predict(img_array)
+        idx = np.argmax(pred)
 
-    img = img.resize((img_height, img_width))
-
-    img_array = np.reshape(img,(1,img_height,img_width,3))
-    pred=model.predict(img_array)
-    idx=np.argmax(pred)
-
-    return jsonify({'result': str(idx)})
+        return jsonify({'result': str(idx)})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 
